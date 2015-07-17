a7b824f7bd03bc601b0699f40cd3fededf6c3589
zhaokc1104 <apatheia1104@163.com>
2015-07-17 14:55:55 +0800

    基本文件

---
+++ wp-content/themes/aries/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8"/>
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <script src="http://lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>
+</head>
+<body>
+    12344
+</body>
+</html>
