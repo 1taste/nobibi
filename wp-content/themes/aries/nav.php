ff041d7557a5a760409780c2caf48b939e6329a2
zhaokc1104 <apatheia1104@163.com>
2015-10-08 15:23:08 +0800

    Merge branch 'master' of https://github.com/zhaokc1104/nobibi
    Conflicts:
    	wp-content/themes/aries/style/common.css

--- wp-content/themes/aries/nav.php
+++ wp-content/themes/aries/nav.php
@@ -13,7 +13,7 @@ Template Name:nav
             <?php
                 $items = wp_get_nav_menu_items('top-menu', array());
                 foreach($items as $key => $item) {
-                    echo '<li><a href="'.$item->url.'" id="nav-'.$item->attr_title.'"></a><span></span></li>';
+                    echo '<li><a class="wave" href="'.$item->url.'" id="nav-'.$item->attr_title.'"></a><span></span></li>';
                 }
             ?>
         </ul>
