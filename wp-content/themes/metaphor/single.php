3ea04f42c5d727a7e6537f5fadbf38334f811123
zhaokc1104 <apatheia1104@163.com>
2016-04-19 20:38:45 +0800

    Merge branch 'master' of https://github.com/zhaokc1104/nobibi
    Conflicts:
    	wp-content/themes/aries/style/common.css

--- wp-content/themes/metaphor/single.php
+++ wp-content/themes/metaphor/single.php
@@ -4,6 +4,26 @@
     <?php
     get_header();
 ?>
+<link href="<?php bloginfo('template_url')?>/plugin/syntaxhighlighter/styles/shCore.css" rel="stylesheet">
+<link href="<?php bloginfo('template_url')?>/plugin/syntaxhighlighter/styles/shThemeRDark.css" rel="stylesheet">
+<script src="<?php bloginfo('template_url')?>/plugin/syntaxhighlighter/scripts/shCore.js"></script>
+<script src="<?php bloginfo('template_url')?>/plugin/syntaxhighlighter/scripts/shBrushJScript.js"></script>
+<script>
+    SyntaxHighlighter.config.clipboardSwf = '<?php bloginfo('template_url')?>/plugin/syntaxhighlighter/scripts/clipboard.swf';
+    SyntaxHighlighter.config.strings = {
+        expandSource : '展开代码',
+        viewSource : null,
+        copyToClipboard : '复制代码',
+        copyToClipboardConfirmation : '代码复制成功',
+        print : null,
+        help: null,
+        alert: '',
+        noBrush: '不能找到刷子: ',
+        brushNotHtmlScript: '刷子没有配置html-script选项',
+        aboutDialog: '<div></div>'
+    };
+    SyntaxHighlighter.all();
+</script>
 </head>
 <body>
 <?php
@@ -42,4 +62,5 @@
 
     </div>
 </article>
+
 <?php get_footer();?>
